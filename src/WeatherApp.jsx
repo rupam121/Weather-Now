aa585822c63e1e327664b2f83b519a820da5b1ca
RUPAM GIRI <103890234+rupam121@users.noreply.github.com>
2024-11-08 14:13:27 +0530

    Update

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -35,7 +35,7 @@ function WeatherApp() {
       />
       <button
         onClick={fetchWeather}
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        className="px-4 py-2 bg-green-500 text-white rounded hover:bg-blue-600"
       >
         Check Weather
       </button>
